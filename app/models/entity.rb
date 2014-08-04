0c6c13fd8c128aa174f6d518ac1c8353d0aa7c16
Rafael Lima Curi <rafaellc25@gmail.com>
2014-08-04 19:39:13 -0300

    Correção das associações entre tags e entities

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,6 @@
 class Entity < ActiveRecord::Base
+  
+  has_many :tags_entities
+  has_many :tags, through: :tags_entities
+  
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ActiveRecord::Base
+  
+  has_many :tags_entities
+  has_many :entities, through: :tags_entities
+  
 end
