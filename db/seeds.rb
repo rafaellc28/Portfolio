2e338b8af2cef26fb51ef244370317709ed7ccb0
Rafael Lima Curi <rafaellc25@gmail.com>
2014-08-05 11:48:25 -0300

    Seed com os entities, um education e alguns tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,13 @@ Entity.create(id: 3, name: "companies")
 Entity.create(id: 4, name: "publications")
 Entity.create(id: 5, name: "certificates")
 Entity.create(id: 6, name: "jobs")
+
+Education.create(id: 1, title: "Mestre em Ciência da Computação", institution: "Universidade Estadual de Campinas", description: "", started_at: "2010-03-01", done_at: "2013-03-31")
+
+Tag.create(id: 1, tag: "Computer Science")
+Tag.create(id: 2, tag: "Game Theory")
+Tag.create(id: 3, tag: "Routing Games")
+
+TagsEntity.create(tag_id: 1, education_id: 1)
+TagsEntity.create(tag_id: 2, education_id: 1)
+TagsEntity.create(tag_id: 3, education_id: 1)
