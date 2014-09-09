87c42c97dd9276f5d75309d80568aee8a4a77741
Rafael Lima Curi <rafaellc25@gmail.com>
2014-09-09 17:47:30 -0300

    Set data-turbolinks to false. set static_assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  config.serve_static_assets = false
   
   # Generate digests for assets URLs.
   config.assets.digest = true
