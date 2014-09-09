46eadb9f6dcf0397ce425a341d5f65717b4db173
Rafael Lima Curi <rafaellc25@gmail.com>
2014-09-09 16:26:39 -0300

    Included index page for users. Still to include show and edit. Corrected authentication token.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,22 @@
 class Admin::UsersController < Admin::ApplicationController
+  
+  def index
+    
+    @user = User.all.first
+    
+    #if @user.name.nil? or @user.name.nil?
+      flash[:warning] = "Please, give a name to the user. It will automatically generate the authentication token!"
+    #end
+    
+    @label_title = "User"
+    @label_name = "Name"
+    @label_email = "Email"
+    @label_authToken = "Authentication Token"
+    @label_edit = "Edit"
+    
+  end
+  
+  def show
+  end
+  
 end
